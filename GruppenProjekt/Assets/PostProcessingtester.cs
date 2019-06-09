68d09dc930bb01accc09b9bb78faa470f7deb66e
adic95 <adi-custovic@hotmail.de>
2019-06-09 16:13:56 +0200

    Futher post processing test

---
+++ GruppenProjekt/Assets/PostProcessingtester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingtester : MonoBehaviour
+{
+    PostProcessVolume m_volume;
+    Vignette m_vignette;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the Post Processing Volume Component and set isGlobal to true
+        m_volume = GetComponent<PostProcessVolume>();
+        m_volume.isGlobal = true;
+
+        m_vignette = ScriptableObject.CreateInstance<Vignette>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
